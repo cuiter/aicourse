8d4c33525384012e9e69a8485af4264fd8dab94a
Casper Uiterwijk <contact@cuiter.me>
2020-03-22 12:43:52 +0100

    Add gradient descent feature scaling and doctests

--- src/linreg.rs
+++ src/linreg.rs
@@ -37,11 +37,12 @@ impl<T : Float> Solver<T> {
         costsum / T::from_u32(n_inputs.get_m() * 2).unwrap()
     }
 
+    /// Performs gradient descent without feature scaling.
     fn train_gradient_descent(n_inputs: &Matrix<T>, outputs: &Matrix<T>) -> Option<Matrix<T>> {
         let n_inputs_trans = n_inputs.transpose();
         let mut current_configuration = Matrix::<T>::zero(n_inputs.get_n(), 1);
 
-        let cost_epsilon = T::from_f32(0.001).unwrap();
+        let cost_epsilon = T::from_f32(0.00001).unwrap();
         let mut learning_rate = T::from_f32(1.0).unwrap();
 
         loop {
@@ -76,6 +77,38 @@ impl<T : Float> Solver<T> {
         Some(current_configuration)
     }
 
+    /// Performs gradient descent with feature scaling.
+    /// Results in more accurate and quicker convergence.
+    fn train_gradient_descent_feature_scaling(n_inputs: &Matrix<T>, outputs: &Matrix<T>) -> Option<Matrix<T>> {
+        let mut inputs_scale = Matrix::one(1, n_inputs.get_n());
+        for n in 0..n_inputs.get_n() {
+            let mut max_value = T::zero();
+            for m in 0..n_inputs.get_m() {
+                if T::abs(n_inputs[(m, n)]) > T::abs(max_value) {
+                    max_value = n_inputs[(m, n)];
+                }
+            }
+            if max_value != T::zero() {
+                inputs_scale[(0, n)] = T::from_f32(1.0).unwrap() / max_value;
+            }
+        }
+
+        let mut scaled_n_inputs = n_inputs.clone();
+        for n in 0..n_inputs.get_n() {
+            for m in 0..n_inputs.get_m() {
+                scaled_n_inputs[(m, n)] = scaled_n_inputs[(m, n)] * inputs_scale[(0, n)];
+            }
+        }
+
+        let mut configuration = Solver::<T>::train_gradient_descent(&scaled_n_inputs, outputs)?;
+
+        for m in 0..configuration.get_m() {
+            configuration[(m, 0)] = configuration[(m, 0)] * inputs_scale[(0, m)];
+        }
+
+        Some(configuration)
+    }
+
     /// Computes an optimal configuration by inverting a matrix.
     /// This may fail because the matrix may not be invertible.
     /// In this case, a technique called SVD is needed to compute a result,
@@ -102,6 +135,25 @@ impl<T : Float> Solver<T> {
     /// The solve method can either be GradientDescent or NormalEquation.
     /// Returns whether the training has succeeded.
     /// If the training has not succeeded, the configuration will be None.
+    /// ```
+    /// let inputs = aicourse::matrix::Matrix::new(3, 2, vec![3.0, 5.0,
+    ///                                                       9.0, 2.0,
+    ///                                                      -3.0, 4.0]);
+    /// let outputs = aicourse::matrix::Matrix::new(3, 1, vec![35.0,
+    ///                                                        92.0,
+    ///                                                       -26.0]);
+    ///
+    /// let correct_configuration = aicourse::matrix::Matrix::new(3, 1, vec![0.0,
+    ///                                                                      10.0,
+    ///                                                                      1.0]);
+    ///
+    /// let mut solver = aicourse::linreg::Solver::new();
+    /// solver.train(&inputs, &outputs, aicourse::linreg::SolveMethod::GradientDescent);
+    /// assert!(solver.get_configuration().approx_eq(&correct_configuration, 0.1));
+    ///
+    /// solver.train(&inputs, &outputs, aicourse::linreg::SolveMethod::NormalEquation);
+    /// assert!(solver.get_configuration().approx_eq(&correct_configuration, 0.1));
+    /// ```
     pub fn train(&mut self, inputs: &Matrix<T>, outputs: &Matrix<T>, method: SolveMethod) -> bool {
         assert_eq!(inputs.get_m(), outputs.get_m());
         assert_eq!(outputs.get_n(), 1);
@@ -110,13 +162,28 @@ impl<T : Float> Solver<T> {
 
         self.configuration = match method {
             SolveMethod::NormalEquation => Solver::<T>::train_normal_equation(&n_inputs, outputs),
-            SolveMethod::GradientDescent => Solver::<T>::train_gradient_descent(&n_inputs, outputs),
+            SolveMethod::GradientDescent => Solver::<T>::train_gradient_descent_feature_scaling(&n_inputs, outputs),
         };
 
         self.configuration != None
     }
 
     /// Runs a prediction on the given inputs to form desired outputs.
+    /// ```
+    /// let inputs = aicourse::matrix::Matrix::new(3, 2, vec![3.0, 5.0,
+    ///                                                       9.0, 2.0,
+    ///                                                      -3.0, 4.0]);
+    /// let outputs = aicourse::matrix::Matrix::new(3, 1, vec![35.0,
+    ///                                                        92.0,
+    ///                                                       -26.0]);
+    ///
+    /// let mut solver = aicourse::linreg::Solver::new();
+    /// solver.train(&inputs, &outputs, aicourse::linreg::SolveMethod::GradientDescent);
+    ///
+    /// let predicted_outputs = solver.run(&inputs);
+    ///
+    /// assert!(predicted_outputs.approx_eq(&outputs, 0.1));
+    /// ```
     pub fn run(&self, inputs: &Matrix<T>) -> Matrix<T> {
         assert!(self.configuration != None, "solver needs to be trained first");
         let n_configuration = self.get_configuration();
@@ -135,12 +202,13 @@ mod tests {
     use super::*;
 
     fn tests_inputs() -> Vec<Matrix<f64>> {
-        vec![Matrix::new(9, 2, vec![50.0, 6.0,
+        vec![
+             Matrix::new(9, 2, vec![50.0, 6.0,
                                     100.0, 5.0,
                                     200.0, 3.0,
                                     400.0, 0.0,
                                     500.0, 0.0,
-                                    600.0, 0.0,
+                                    600.0, 500.0,
                                     700.0, 0.0,
                                     800.0, 0.0,
                                    -100.0, 0.0]),
@@ -150,12 +218,13 @@ mod tests {
     }
 
     fn tests_outputs() -> Vec<Matrix<f64>> {
-        vec![Matrix::new(9, 1, vec![1512.0,
+        vec![
+             Matrix::new(9, 1, vec![1512.0,
                                     2010.0,
                                     3006.0,
                                     5000.0,
                                     6000.0,
-                                    7000.0,
+                                    8000.0,
                                     8000.0,
                                     9000.0,
                                        0.0]),
@@ -165,7 +234,8 @@ mod tests {
     }
 
     fn tests_configuration() -> Vec<Matrix<f64>> {
-        vec![Matrix::new(3, 1, vec![1000.0,
+        vec![
+             Matrix::new(3, 1, vec![1000.0,
                                     10.0,
                                     2.0]),
              Matrix::new(2, 1, vec![0.0,
