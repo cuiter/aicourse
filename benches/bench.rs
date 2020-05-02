c4aa88457446db15e5b8efe98c7527971ff750bf
Casper Uiterwijk <contact@cuiter.me>
2020-05-02 11:53:58 +0200

    Optimize load_idx by decoding all elements at once

--- benches/bench.rs
+++ benches/bench.rs
@@ -5,7 +5,7 @@ use aicourse::matrix::{Matrix, load_idx};
 use bencher::{Bencher, black_box};
 use std::fs;
 
-fn matrix_new(bench: &mut Bencher, rows_cols: u32) {
+fn matrix_zero(bench: &mut Bencher, rows_cols: u32) {
     let mut sum_size = 0;
     bench.iter(|| {
         let matrix = Matrix::<f32>::zero(rows_cols, rows_cols);
@@ -14,21 +14,29 @@ fn matrix_new(bench: &mut Bencher, rows_cols: u32) {
     black_box(sum_size);
 }
 
-fn matrix_new_100_100(bench: &mut Bencher) {
-    matrix_new(bench, 100);
+fn matrix_zero_100_100(bench: &mut Bencher) {
+    matrix_zero(bench, 100);
 }
 
-fn matrix_new_1000_1000(bench: &mut Bencher) {
-    matrix_new(bench, 1000);
+fn matrix_zero_1000_1000(bench: &mut Bencher) {
+    matrix_zero(bench, 1000);
 }
 
-fn load_idx_mnist_labels(bench: &mut Bencher) {
-    let train_images_data = fs::read("datasets/mnist/train-labels-idx1-ubyte").unwrap();
+fn load_idx_mnist_train_labels(bench: &mut Bencher) {
+    let data = fs::read("datasets/mnist/train-labels-idx1-ubyte").unwrap();
     bench.iter(|| {
-        let matrix = load_idx::<f32>(&train_images_data).unwrap();
+        let matrix = load_idx::<f32>(&data).unwrap();
         black_box(matrix.sum());
     });
 }
 
-benchmark_group!(benches, matrix_new_100_100, matrix_new_1000_1000, load_idx_mnist_labels);
+fn load_idx_mnist_test_images(bench: &mut Bencher) {
+    let data = fs::read("datasets/mnist/t10k-images-idx3-ubyte").unwrap();
+    bench.iter(|| {
+        let matrix = load_idx::<f32>(&data).unwrap();
+        black_box(matrix.sum());
+    });
+}
+
+benchmark_group!(benches, matrix_zero_100_100, matrix_zero_1000_1000, load_idx_mnist_train_labels, load_idx_mnist_test_images);
 benchmark_main!(benches);
