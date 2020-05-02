b58c078d84fb12f3c9785cf5ca0dcc22439c0027
cuiter <casper.uiterwijk@gmail.com>
2020-05-02 11:53:58 +0200

    Optimize load_idx by decoding all elements at once

--- src/matrix/idx.rs
+++ src/matrix/idx.rs
@@ -1,6 +1,6 @@
 use super::matrix::{Float, Matrix};
 use std::convert::TryInto;
-use std::io::{Error, ErrorKind};
+use std::io::{Result, Error, ErrorKind};
 
 #[derive(Copy, Clone)]
 enum DataType {
@@ -29,7 +29,7 @@ fn data_type_size(data_type: DataType) -> usize {
     }
 }
 
-fn read_u32(data: &Vec<u8>, index: usize) -> Result<u32, Error> {
+fn read_u32(data: &Vec<u8>, index: usize) -> Result<u32> {
     if index > data.len() - 4 {
         return Err(Error::new(ErrorKind::InvalidData, "index is out of range"));
     }
@@ -37,34 +37,43 @@ fn read_u32(data: &Vec<u8>, index: usize) -> Result<u32, Error> {
     Ok(u32::from_be_bytes(word))
 }
 
-fn read_element<T: Float>(data: &Vec<u8>, data_type: DataType, index: usize) -> Result<T, Error> {
-    let error = Error::new(ErrorKind::InvalidData, "data is corrupted");
-    match data_type {
-        DataType::U8 => T::from_u8(data[index]).ok_or(error),
+fn read_element<T: Float>(element_data: &[u8], data_type: DataType) -> T {
+    let convert_result = match data_type {
+        DataType::U8 => T::from_u8(element_data[0]),
         DataType::I8 => T::from_i8(i8::from_be_bytes(
-            data[index..(index + 1)].try_into().unwrap(),
-        ))
-        .ok_or(error),
+            element_data.try_into().unwrap(),
+        )),
         DataType::I16 => T::from_i16(i16::from_be_bytes(
-            data[index..(index + 2)].try_into().unwrap(),
-        ))
-        .ok_or(error),
+            element_data.try_into().unwrap(),
+        )),
         DataType::I32 => T::from_i32(i32::from_be_bytes(
-            data[index..(index + 4)].try_into().unwrap(),
-        ))
-        .ok_or(error),
+            element_data.try_into().unwrap(),
+        )),
         DataType::F32 => T::from_f32(f32::from_bits(u32::from_be_bytes(
-            data[index..(index + 4)].try_into().unwrap(),
-        )))
-        .ok_or(error),
+            element_data.try_into().unwrap(),
+        ))),
         DataType::F64 => T::from_f64(f64::from_bits(u64::from_be_bytes(
-            data[index..(index + 8)].try_into().unwrap(),
-        )))
-        .ok_or(error),
-    }
+            element_data.try_into().unwrap(),
+        ))),
+    };
+    // For performance reasons, don't return a Result or panic.
+    // Speculation: If the parsing failed, which could only happen
+    // on a floating-point number, the result can be set to NaN.
+    convert_result.unwrap_or(T::nan())
 }
 
-fn read_magic_number(data: &Vec<u8>) -> Result<MagicNumber, Error> {
+fn read_elements<T: Float>(data: &Vec<u8>, magic: MagicNumber, n_items: usize) -> Vec<T> {
+    let mut elements = Vec::with_capacity(n_items);
+    let elements_data = &data[(4 + 4 * magic.dimensions as usize)..];
+
+    for element_data in elements_data.chunks(data_type_size(magic.data_type)) {
+        elements.push(read_element(element_data, magic.data_type));
+    }
+
+    elements
+}
+
+fn read_magic_number(data: &Vec<u8>) -> Result<MagicNumber> {
     if data.len() < 9 {
         return Err(Error::new(
             ErrorKind::InvalidData,
@@ -108,7 +117,7 @@ fn read_magic_number(data: &Vec<u8>) -> Result<MagicNumber, Error> {
     })
 }
 
-fn read_dimension_sizes(data: &Vec<u8>, dimensions: u8) -> Result<Vec<usize>, Error> {
+fn read_dimension_sizes(data: &Vec<u8>, dimensions: u8) -> Result<Vec<usize>> {
     if data.len() < 4 + dimensions as usize * 4 {
         return Err(Error::new(
             ErrorKind::InvalidData,
@@ -128,7 +137,7 @@ fn read_matrix_data<T: Float>(
     data: &Vec<u8>,
     magic: MagicNumber,
     dimension_sizes: &Vec<usize>,
-) -> Result<Matrix<T>, Error> {
+) -> Result<Matrix<T>> {
     let expected_data_len = 4
         + magic.dimensions as usize * 4
         + data_type_size(magic.data_type) * dimension_sizes.iter().product::<usize>();
@@ -146,22 +155,16 @@ fn read_matrix_data<T: Float>(
     let n_rows = dimension_sizes[0];
     let n_cols: usize = dimension_sizes[1..].iter().product();
 
-    let mut matrix = Matrix::zero(n_rows as u32, n_cols as u32);
-    for m in 0..n_rows {
-        for n in 0..n_cols {
-            let index = 4
-                + 4 * magic.dimensions as usize
-                + data_type_size(magic.data_type) * (m * n_cols + n);
-            matrix[(m as u32, n as u32)] = read_element(data, magic.data_type, index)?;
-        }
-    }
+    let elements = read_elements(data, magic, n_rows * n_cols);
+
+    let matrix = Matrix::new(n_rows as u32, n_cols as u32, elements);
 
     Ok(matrix)
 }
 
 /// Loads the matrix data from a byte array in the IDX format.
 /// For details, see: http://www.fon.hum.uva.nl/praat/manual/IDX_file_format.html
-pub fn load_idx<T: Float>(data: &Vec<u8>) -> Result<Matrix<T>, Error> {
+pub fn load_idx<T: Float>(data: &Vec<u8>) -> Result<Matrix<T>> {
     let magic = read_magic_number(data)?;
 
     let dimension_sizes = read_dimension_sizes(data, magic.dimensions)?;
